a476d73e8002975ba29c688ec805f3894aedd9c4
Pavel <48916290+kurylapavel@users.noreply.github.com>
2020-10-13 17:48:11 +0200

    comit

---
+++ Git/Project_lar/resources/views/szablon.blade.php
@@ -0,0 +1,13 @@
+@extends('site')
+
+@section('title',' strona www')
+    
+
+@section('sidebar')
+    @parent
+    <p>doloczony do gl paskabocznego</p>
+@endsection
+
+@section('content')
+    <p>Tresc strony</p>
+@endsection
--- Git/Project_lar/routes/web.php
+++ Git/Project_lar/routes/web.php
@@ -16,3 +16,80 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cdv', function(){
+    //return view ('cdv');
+    //return 'cdv';
+    //return ['name' => 'Anna','surname'=>'kowalska'];
+
+    return view ('/cdv',['name' => 'Anna','surname'=>'kowalska']);
+
+});
+
+
+Route::get('/pages/{x}', function ($x) {
+    $pages = [
+        'about' => 'Strona CDV',
+        'contact' => 'poznan',
+        'home' => 'strona domova'
+    ];
+    return $pages[$x];
+});
+
+// Route::get('/addres/{city}', function (String $city) {
+//     echo "Miasto: $city";
+// });
+
+// Route::get('/addres/{city}/{street}', function (String $city, String $street) {
+//     echo <<< ADRESS
+//     Miasto: $city<br>
+//     Ulica : $street<br>
+// ADRESS;
+// });
+
+Route::get('/address/{city?}/{street?}/{zipCode?}', function (String $city = "brak danych", String $street="-", int $zipCode = null) {
+    $zipCode = substr($zipCode,0,2).'-'.substr($zipCode,2,3);
+    echo <<< ADRESS
+    Kod pocztowy: $zipCode <br>
+    Miasto: $city<br>
+    Ulica : $street<br>
+ADRESS;
+}) -> name('address');
+
+Route::redirect('/adres/{city?}/{street?}/{zipCode?}','/address');
+
+Route::prefix('admin')->group(function(){
+    Route::get('/home/{name}',function (String $name){
+        echo "Witaj $name na stronie administracyjniej";
+    });
+
+    Route::get('users',function(){
+        echo "<h3>Uzytkownicy systemu</h3>";
+    });
+});
+
+Route::redirect('/admin/{name}','/admin/home/{name}');
+
+Route::get('/user/{name}/{age?}',function(String $name,int $age = null){
+    echo "<h3>Dane</h3>";
+
+})->where(['name'=>'[A-Za-z]+', 'age' =>'[0-9]+']);
+
+
+
+Route::get('/city', function(){
+    return view ('city',['name' => 'Anna','surname'=>'kowalska','city'=>'Poznań']);
+});
+
+Route::get('/table', function(){
+    $car =[
+        ['model'=> 'Ferrari','mark'=> 'F430','color'=>'red'],
+        ['model'=> 'Fiat','mark'=> '126p','color'=>'white'],
+        ['model'=> 'Porsche','mark'=> 'Panamera','color'=>'black']
+    ];
+    return view ('table',['car'=> $car]);
+});
+
+Route::get('/blade', function(){
+    return view ('szablon');
+});
+
